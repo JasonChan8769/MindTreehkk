586d27f3ad0487e7dca80ffad16f2900efc53f94
JasonChan8769 <chankaihong343@gmail.com>
2025-11-29 01:52:21 +0800

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,16 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: 請去 Firebase Console > Project Settings > General > Your apps
-// 把那邊的 config 複製過來替換下面這些內容
+// 這是從你的截圖中提取的真實設定
 const firebaseConfig = {
-  apiKey: "AIzaSy...",  
-  authDomain: "mindtree-hk.firebaseapp.com", 
-  projectId: "mindtree-hk", 
-  storageBucket: "mindtree-hk.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyB0abQmyf4vALgQ3XNM_we5B0JCfrteZ4I",
+  authDomain: "mindtreehk.firebaseapp.com",
+  projectId: "mindtreehk",
+  storageBucket: "mindtreehk.firebasestorage.app",
+  messagingSenderId: "326871687350",
+  appId: "1:326871687350:web:92ce082c58f80ef74b8617",
+  measurementId: "G-R6TVNF43T4"
 };
 
+// 初始化 Firebase
 const app = initializeApp(firebaseConfig);
+
+// 匯出資料庫功能，讓其他檔案 (AppContext) 可以使用
 export const db = getFirestore(app);
